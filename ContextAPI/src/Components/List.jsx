53918628b25ddece495397630db66d947b9521e6
Anuj Singh <editingwithanuj@gmail.com>
2024-01-09 20:48:41 +0530

    delete functionality added

--- ContextAPI/src/Components/List.jsx
+++ ContextAPI/src/Components/List.jsx
@@ -1,14 +1,39 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { ItemContext } from "../Store/ItemContext";
 
 const List = () => {
   const data = useContext(ItemContext);
   const items = data.StoreItems;
 
+  useEffect(() => {
+    setDisplayItems(items);
+  }, [items]);
+
+  const [displayItems, setDisplayItems] = useState([]);
+
+  const handleDelete = (key) => {
+    var temp = displayItems.filter((val, index) => {
+      if (!(index == key)) {
+        return val;
+      }
+    });
+    setDisplayItems(temp);
+  };
+
   return (
     <>
-      {items.map((val, index) => {
-        return <div key={index}> {val} </div>;
+      {displayItems.map((val, index) => {
+        return (
+          <div className="ItemContainer" key={index}>
+            <div key={index}> {val} </div>
+            <button
+              style={{ marginLeft: "10px" }}
+              onClick={() => handleDelete(index)}
+            >
+              Delete
+            </button>
+          </div>
+        );
       })}
     </>
   );
